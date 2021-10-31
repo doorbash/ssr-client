688f4c4b355bdb34d07617afa8f1dd0db4406502
doorbash <milad.doorbash@gmail.com>
2021-10-31 14:10:47 +0330

    fix http headers issue

--- http_handler.go
+++ http_handler.go
@@ -5,23 +5,9 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"strings"
 
 	"github.com/nadoo/glider/proxy"
 )
-
-// Hop-by-hop headers. These are removed when sent to the backend.
-// http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html
-var hopHeaders = []string{
-	"Connection",
-	"Keep-Alive",
-	"Proxy-Authenticate",
-	"Proxy-Authorization",
-	"Te", // canonicalized version of "TE"
-	"Trailers",
-	"Transfer-Encoding",
-	"Upgrade",
-}
 
 func copyHeader(dst, src http.Header) {
 	for k, vv := range src {
@@ -29,22 +15,6 @@ func copyHeader(dst, src http.Header) {
 			dst.Add(k, v)
 		}
 	}
-}
-
-func delHopHeaders(header http.Header) {
-	for _, h := range hopHeaders {
-		header.Del(h)
-	}
-}
-
-func appendHostToXForwardHeader(header http.Header, host string) {
-	// If we aren't the first proxy retain prior
-	// X-Forwarded-For information as a comma+space
-	// separated list and fold multiple headers into one.
-	if prior, ok := header["X-Forwarded-For"]; ok {
-		host = strings.Join(prior, ", ") + ", " + host
-	}
-	header.Set("X-Forwarded-For", host)
 }
 
 type HttpHandler struct {
@@ -104,11 +74,6 @@ func (s *HttpHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		},
 	}
 	r.RequestURI = ""
-	delHopHeaders(r.Header)
-
-	if clientIP, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
-		appendHostToXForwardHeader(r.Header, clientIP)
-	}
 	resp, err := client.Do(r)
 	if err != nil {
 		http.Error(rw, "Server Error", http.StatusInternalServerError)
@@ -116,7 +81,6 @@ func (s *HttpHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer resp.Body.Close()
-	delHopHeaders(resp.Header)
 
 	copyHeader(rw.Header(), resp.Header)
 	rw.WriteHeader(resp.StatusCode)
